0a0cfb8d270016154e2309b2fb23285cdf358609
Shashwat Choudhry <choudhryshashwat29@gmail.com>
2026-04-23 18:03:42 -0500

    futbolV1

---
+++ code/futbolV1/checkingData.r
@@ -0,0 +1,6 @@
+install.packages("worldfootballR", repos="http://cran.us.r-project.org")
+install.packages("dplyr", repos="http://cran.us.r-project.org")
+
+
+library(worldfootballR)
+library(dplyr)
