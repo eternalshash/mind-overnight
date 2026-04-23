36791d90777bf3cabaf3d40f65486d8772e38fac
Shashwat Choudhry <choudhryshashwat29@gmail.com>
2026-04-23 18:27:15 -0500

    updates- Issue with getting data

--- code/futbolV1/checkingData.r
+++ code/futbolV1/checkingData.r
@@ -1,6 +1,14 @@
-install.packages("worldfootballR", repos="http://cran.us.r-project.org")
-install.packages("dplyr", repos="http://cran.us.r-project.org")
-
-
 library(worldfootballR)
-library(dplyr)
+
+print("Loading Match Details for EPL...")
+
+# This pulls the massive dataset containing lineups, stats, and match ratings
+epl_match_details <- load_fotmob_match_details(
+  country = "ENG", 
+  league_name = "Premier League"
+)
+
+# Export straight to CSV so we can jump into Python
+write.csv(epl_match_details, "epl_match_details.csv", row.names = FALSE)
+
+print("Match data exported successfully! Ready for Python.")
