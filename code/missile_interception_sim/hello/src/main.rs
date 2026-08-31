5c2cf6bfbea25ef32701d4693b0673f222f7bc81
Shashwat Choudhry <choudhryshashwat29@gmail.com>
2026-08-30 22:49:13 -0500

    rust Inference update

--- code/missile_interception_sim/hello/src/main.rs
+++ code/missile_interception_sim/hello/src/main.rs
@@ -14,5 +14,33 @@ fn main() {
     println!("{}", logical);
     println!("{}", a_float);
 
+    let default_float = 3.0; // 'f64'
+    let default_integer = 7; // 'i32'
+
+    //type inferred from context 
+    let mut inferred_type = 12;  // the type i64 is inferred from another line
+    inferred type = 4294967296i64;
+
+    // rust by default is interpreting the interger literal as a default type
+    // being i32 (a signed 32 bit integer)
+    // Rust uses a bidirectional type inference
+    // so it won't just read line 21 by itself 
+
+    /*
+    Inference in rust :
+        line 21 -> let mut inferred_type =12;
+        rust is creating a mutable variable, which sees 12 
+        but it leaves the exacct type undecided for a moment 
+        Rust knows it is a integer type
+        once line 22 is executed 
+        a explcit type is assigned to the inferred_type
+        In Rust a varibale type can never change at runtime
+        
+    
+    
+     */
+
+
+
 
 }
