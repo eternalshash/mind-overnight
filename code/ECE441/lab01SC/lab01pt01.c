ef5a36123f64583a71a7dbbe0cd00d5827f3d4af
Shashwat Choudhry <choudhryshashwat29@gmail.com>
2026-09-09 22:44:58 -0500

    feat(pt1): add Serial init and startup telemetry

--- code/ECE441/lab01SC/lab01pt01.c
+++ code/ECE441/lab01SC/lab01pt01.c
@@ -4,6 +4,8 @@ void setup(void) {
     DDRD |= (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
     DDRB &= ~((1 << 4) | (1 << 5));
     PORTD = (1 << 4) | (1 << 5);
+    Serial.begin(115200);
+    Serial.println(F("[TELEMETRY][PT1] System initialized. State: IDLE (NS: GREEN, EW: RED)"));
 }
 
 void loop(void) {
