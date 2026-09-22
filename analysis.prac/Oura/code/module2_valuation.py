5156e266bd85979d6bd614e900fd4e68aff9f8bc
Shashwat Choudhry <choudhryshashwat29@gmail.com>
2026-09-21 19:32:01 -0500

    Mod 2 pt 2

--- analysis.prac/Oura/code/module2_valuation.py
+++ analysis.prac/Oura/code/module2_valuation.py
@@ -8,3 +8,8 @@ def monte_carlo_dcf(simulations=10000):
     monthly_churn = np.random.uniform(0.015, 0.05, simulations)
     
     valuations = units_growth * 1000 * (1 - monthly_churn) * 15 # mock formula
+    return valuations
+
+def plot_valuation(valuations):
+    plt.figure(figsize=(10,6))
+    plt.hist(valuations, bins=50, color='blue', alpha=0.7)
