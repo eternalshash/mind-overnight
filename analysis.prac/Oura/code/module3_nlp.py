ff91fd97d2f8ceb6a5182805cb5f2deae15bb6e3
Shashwat Choudhry <choudhryshashwat29@gmail.com>
2026-09-21 19:32:07 -0500

    Mod 3 pt 1

--- analysis.prac/Oura/code/module3_nlp.py
+++ analysis.prac/Oura/code/module3_nlp.py
@@ -3,3 +3,8 @@ import matplotlib.pyplot as plt
 import os
 import seaborn as sns
 import numpy as np
+
+def scrape_and_parse_reviews():
+    # Mocking NLP output for Tech Reviews
+    sources = ['The Verge', 'Wired', 'DC Rainmaker', 'Bloomberg']
+    aspects = ['Hardware', 'Subscription Paywall', 'Biometrics', 'Ecosystem']
