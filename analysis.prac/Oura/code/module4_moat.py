019b9af4cdf685563f7e0a2feeee3b5df7391b29
Shashwat Choudhry <choudhryshashwat29@gmail.com>
2026-09-21 19:32:15 -0500

    Mod 4 pt 1

--- analysis.prac/Oura/code/module4_moat.py
+++ analysis.prac/Oura/code/module4_moat.py
@@ -3,3 +3,8 @@ import matplotlib.pyplot as plt
 import os
 
 def galaxy_ring_stress_test(base_arr, fee_reduction_pct):
+    stressed_arr = base_arr * (1 - fee_reduction_pct)
+    valuation_impact = stressed_arr * 10 # 10x multiple
+    return stressed_arr, valuation_impact
+
+def b2b_moat_scoring():
